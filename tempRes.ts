4db5fcfa91be8c5c32d5b8198f88ae8914349ed7
Amir Mohammad <amirfanweb@gmail.com>
2024-03-19 16:32:25 +0330

    renew docs

---
+++ tempRes.ts
@@ -0,0 +1,22 @@
+const rate = {
+  image: {
+    data: {
+      image_url: "",
+    },
+  },
+  rate: {
+    data: {
+      star_count: 4.25,
+    },
+  },
+  comment: {
+    data: {
+      value: "the comment",
+    },
+  },
+  author: {
+    data: {
+      value: "your name",
+    },
+  },
+};
